7a8c349cf63f7238b21140373cb0c0573010bdc2
DARKSL-IME <imeshbandara7@gmail.com>
2023-12-03 03:20:54 +0530

    Add the button for alignments to index.html and add function for javascript file

--- texteditor.js
+++ texteditor.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('colorPickerButton').addEventListener('click',colorPickeropen);
+});
+
+
+
+
+
 function clickBold() {
     document.execCommand('bold', true, null);
 }
@@ -10,4 +18,25 @@ function clickUnderline() {
 
 function clickItalic() {
     document.execCommand('italic', true, null);
+}
+
+function colorPickeropen() {
+    const colorPicker = document.getElementById('fontColorPicker');
+    colorPicker.click();
+}
+
+function changeFontColor() {
+    const colorPicker = document.getElementById('fontColorPicker');
+    const selectedColor = colorPicker.value;
+    document.execCommand('foreColor', true, selectedColor);
+}
+
+
+function clickTextAlignment(align) {
+    if (['left', 'center', 'right', 'justify'].includes(align)) {
+        document.getElementById('editor').style.textAlign = align;
+    } else {
+        console.error('Invalid alignment value');
+    }
+
 }
